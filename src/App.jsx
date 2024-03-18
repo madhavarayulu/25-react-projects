83dd36637f4eaae32f9d37ad3f3e6fdc53b08060
madhavarayulu <madhavarayulu@gmail.com>
2024-03-19 01:31:01 +0530

    Project set up & complete accordion

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Accordion from './components';
+
+function App() {
+  return (
+    <div className='app'>
+      <Accordion />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { data } from './data';
+import './styles.css';
+
+export default function Accordion() {
+  const [select, setSelect] = useState(null);
+  const [enableMultiSelect, setEnableMultiSelect] = useState(false);
+  const [multipleSelection, setMultipleSelection] = useState([]);
+
+  function handleSingleSelection(currentId) {
+    setSelect(currentId === select ? null : currentId);
+  }
+
+  function handleMultiSelection(currentId) {
+    let copyOfMultiSelection = [...multipleSelection];
+    const findIndexOfCurrentId = copyOfMultiSelection.indexOf(currentId);
+    if (findIndexOfCurrentId === -1) {
+      copyOfMultiSelection.push(currentId);
+    } else {
+      copyOfMultiSelection.splice(findIndexOfCurrentId, 1);
+    }
+    setMultipleSelection(copyOfMultiSelection);
+  }
+
+  function renderAccordionItem(dataItem) {
+    return (
+      <div className="item">
+        <div
+          onClick={
+            enableMultiSelect
+              ? () => handleMultiSelection(dataItem.id)
+              : () => handleSingleSelection(dataItem.id)
+          }
+          className="title"
+        >
+          <h3>{dataItem.question}</h3>
+          <span>+</span>
+        </div>
+        {select === dataItem.id ||
+        multipleSelection.indexOf(dataItem.id) !== -1 ? (
+          <div className="content">{dataItem.answer}</div>
+        ) : null}
+      </div>
+    );
+  }
+
+  return (
+    <div className="wrapper">
+      <button onClick={() => setEnableMultiSelect(!enableMultiSelect)}>
+        Enable Multi Selection
+      </button>
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map(renderAccordionItem)
+        ) : (
+          <div>No data found!</div>
+        )}
+      </div>
+    </div>
+  );
+}
